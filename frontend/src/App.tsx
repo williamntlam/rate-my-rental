59fde9268bd586141b9ae63f06e61045ff2241f8
William Lam <williamntlam@gmail.com>
2024-08-06 14:57:25 -0400

    feat: added contact us page author: William Lam<williamntlam@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import AboutUs from "./static/AboutUs";
+import PrivacyPolicy from "./static/PrivatePolicy";
+import TermsOfService from "./static/TermsOfService";
+import Contact from "./static/Contact";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <AboutUs />
+      <Contact />
       <Footer />
     </div>
   );
--- frontend/src/static/Contact.tsx
+++ frontend/src/static/Contact.tsx
@@ -1,5 +1,97 @@
-const Contact = () => {
-  return <div>Contact</div>;
+import React, { useState } from "react";
+
+const Contact: React.FC = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    // Need to set up backend for this.
+    console.log(name);
+    console.log(email);
+    console.log(message);
+  };
+
+  return (
+    <div className="min-h-screen p-6 bg-gray-100">
+      <div className="max-w-4xl mx-auto bg-white shadow-md rounded-lg p-8">
+        <h1 className="text-3xl font-bold mb-4 text-gray-800">Contact Us</h1>
+        <p className="text-gray-700 mb-6">
+          If you have any questions, concerns, or feedback, we'd love to hear
+          from you! Please use the form below to get in touch with us, and we'll
+          get back to you as soon as possible.
+        </p>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleNameChange}
+              placeholder="Your name"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleEmailChange}
+              placeholder="Your email address"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="message"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Your message"
+              onChange={handleMessageChange}
+              rows={5}
+            ></textarea>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;

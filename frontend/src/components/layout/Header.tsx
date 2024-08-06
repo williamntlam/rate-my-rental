3192607b1da1f615f072d09fdf9557ef26e7cc05
William Lam <williamntlam@gmail.com>
2024-08-06 11:21:41 -0400

    feat: added header icon in top left author: William Lam<williamntlam@gmail.com>

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,7 +1,11 @@
+import headerIcon from "/headerIcon.png";
+
 const Header = () => {
   return (
-    <header className="w-100 bg-orange-600 h-20 flex justify-between">
-      <div>Icon</div>
+    <header className="flex items-center justify-between p-4 bg-orange-600 text-white h-16">
+      <div>
+        <img src={headerIcon} className="p-6 w-24 h-24 object-contain" />
+      </div>
       <div>Buttons in middle</div>
       <div>Buttons on right</div>
     </header>

7f4e4fca5e80be8aed55005c000c35f25d6e7761
William Lam <williamntlam@gmail.com>
2024-08-06 21:18:39 -0400

    feat: moved save button to righthand side of screen. author: William Lam<williamntlam@gmail.com>

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
 
       {/* Messenger Buttons, Profile Buttons */}
       <div className="flex justify-between space-x-4">
-        <button className="bg-white rounded-full p-2 h-10 w-10 ml-2 hover:bg-gray-400">
+        <button className="bg-white rounded-full p-2 h-10 w-10 hover:bg-gray-400">
           <img
             src={saveIcon}
             className="object-contain h-full w-full"
@@ -29,7 +29,7 @@ const Header = () => {
           <img
             src={profileIcon}
             className="object-contain h-full w-full"
-            alt="Profile Icon"
+            alt="Pro Icon"
           />
         </button>
       </div>
